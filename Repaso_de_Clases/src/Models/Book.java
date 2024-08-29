663fdf5e667eb4ec5741ca6e7249a8bd010f15a7
Rusbel <juanrusbelusca@gmail.com>
2024-08-29 12:48:37 -0500

    Subir Clases

---
+++ Repaso_de_Clases/src/Models/Book.java
@@ -0,0 +1,81 @@
+package Models;
+
+import java.util.Date;
+
+public class Book {
+    private int id;
+    private String codeBook;
+    private String name;
+    private String title;
+    private String publisher;
+    private char sbn;
+    private Date createAt;
+    private Date updateAt;
+
+    public Book() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodeBook() {
+        return codeBook;
+    }
+
+    public void setCodeBook(String codeBook) {
+        this.codeBook = codeBook;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public char getSbn() {
+        return sbn;
+    }
+
+    public void setSbn(char sbn) {
+        this.sbn = sbn;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
