c76ae73d856a0e8e37c47f0717937e92952d11ae
Rusbel <juanrusbelusca@gmail.com>
2024-08-29 12:56:54 -0500

    Subir Clases Author

---
+++ Repaso_de_Clases/src/Models/Author.java
@@ -0,0 +1,82 @@
+package Models;
+
+import java.util.Date;
+
+public class Author {
+    private int id;
+    private String name;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Date createAt;
+    private Date updateAt;
+
+    public Author() {
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
